4100a898a91d24819adeee09e937957f38aadf04
samirsahoo <sameer_sahoo@apple.com>
2020-05-08 13:31:53 +0530

    adding nosetest example

---
+++ nose/test_mymath.py
@@ -0,0 +1,28 @@
+import nose
+from mymath import *
+from nose.tools import nottest
+
+def test_add_integers():
+    assert add(5, 3) == 8
+    
+def test_add_integers_zero():
+    assert add(3, 0) == 3
+    
+def test_add_floats():
+    assert add(1.5, 2.5) == 4
+
+def test_add_strings():
+    nose.tools.assert_raises(AssertionError, add, 'paul', 'carol')
+    
+@nottest                            # Don't test a test
+def test_divide_integers_even():
+    assert divide(2, 10) == 0.2
+    
+def test_divide_integers_repetant():
+    nose.tools.assert_almost_equal(divide(1, 3), 0.33333333, 7)
+
+if __name__ == '__main__':   
+    nose.run()
+
+# How to run: nosetests -sv test_mymath.py
+
